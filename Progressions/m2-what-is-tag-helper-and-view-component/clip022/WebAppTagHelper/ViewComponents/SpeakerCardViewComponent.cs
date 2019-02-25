b7c07652c1a5f9941aa2f77f2ec4dc9c828cc678
pkellner <peter@peterkellner.net>
2019-02-25 09:20:20 -0800

    working progress

---
+++ Progressions/m2-what-is-tag-helper-and-view-component/clip022/WebAppTagHelper/ViewComponents/SpeakerCardViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppTagHelper.Models;
+
+namespace WebAppTagHelper.ViewComponents
+{
+    public class SpeakerCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            Speaker speaker)
+        {
+            return View(speaker);
+        }
+    }
+}
