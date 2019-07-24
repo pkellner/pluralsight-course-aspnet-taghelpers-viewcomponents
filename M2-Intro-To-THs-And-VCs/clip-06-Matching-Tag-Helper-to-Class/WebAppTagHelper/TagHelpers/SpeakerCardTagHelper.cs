d17061e311e07aaef95a0d5befbbd7027b04781f
pkellner <peter@peterkellner.net>
2019-07-23 17:53:42 -0700

    work in progress

---
+++ M2-Intro-To-THs-And-VCs/clip-06-Matching-Tag-Helper-to-Class/WebAppTagHelper/TagHelpers/SpeakerCardTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("tag-name")]
+    public class SpeakerCardTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
---
+++ M2-Intro-To-THs-And-VCs/clip-07-Rendering-Tag-Helper-Output/WebAppTagHelper/TagHelpers/SpeakerCardTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    //[HtmlTargetElement("tag-name")]
+    public class SpeakerCardTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
