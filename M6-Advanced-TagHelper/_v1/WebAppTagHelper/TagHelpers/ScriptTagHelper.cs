581842154ca1d5fe9a880f9885cfe6a1ec772ffe
pkellner <peter@peterkellner.net>
2019-08-16 14:09:14 -0700

    wip

---
+++ M6-Advanced-TagHelper/_v1/WebAppTagHelper/TagHelpers/ScriptTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("script", Attributes = "src")]
+    public class ScriptTagHelper : TagHelper
+    {
+        private readonly IScriptManager _scriptManager;
+
+        public ScriptTagHelper(IScriptManager scriptManager)
+        {
+            _scriptManager = scriptManager;
+        }
+
+        public string IncludeOrderPriority { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            // needed because of builtin tag helper that looks at src
+            var src = output.Attributes["src"].Value.ToString();
+            _scriptManager.AddScript(new ScriptReference(src, Convert.ToInt32(IncludeOrderPriority)));
+
+            await output.GetChildContentAsync();
+            output.SuppressOutput();
+        }
+    }
+}
---
+++ M6-Advanced-TagHelper/clip-02/TagHelpers/ModExpTagHelper.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    [HtmlTargetElement("mod-exp")]
+    public class ModExpTagHelper : TagHelper
+    {
+        public ModelExpression HelperFor { get; set; }
+
+        public override void Process(TagHelperContext context, 
+            TagHelperOutput output)
+        {
+            var str = HelperFor == null ? "" :
+                " Name: " + HelperFor.Name +
+                " Model: " + HelperFor.Model;
+            output.Content.SetContent(str);
+        }
+    }
+}
